2d1536c58dc84ae640a73a8996771e2c28d24766
1-blue <ghksaud55@naver.com>
2020-10-07 14:53:57 +0900

    sound test

---
+++ DelayManager.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <ctime>
+
+using namespace std;
+
+class DelayManager
+{
+private:
+	clock_t startTime;
+	int delayTime;
+public:
+	DelayManager();
+
+	void SetDelayTime(int time);
+	int GetDelayTime();
+
+	void SetStartTime();
+
+	bool CkEndDelay();
+
+};
+
