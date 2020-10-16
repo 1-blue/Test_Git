9c2e729bf0fd4644d7921218aef9e3069d0c3be6
1-blue <ghksaud55@naver.com>
2020-10-17 01:34:51 +0900

    자료구조추가

--- my_test_Project/main.cpp
+++ my_test_Project/main.cpp
@@ -1,16 +1,9 @@
-#include "_Vector.h"
-#include "_Vector.cpp"
+
 
 int main(void)
 {
-	_Vector<string> vector(3);
 
-	vector.Push("apple");
-	vector.Push("blue");
-	vector.Push("color");
 
-	for (int i = 0; i < 3; i++)
-		cout << vector[i] << endl;
 
 	return 0;
 }
