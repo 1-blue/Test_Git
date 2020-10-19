463799be272c62cc001a24a4d419988bdc55d21d
1-blue <ghksaud55@naver.com>
2020-10-20 04:31:05 +0900

    test

--- my_test_Project/main.cpp
+++ my_test_Project/main.cpp
@@ -1,8 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+	char c[3] = { 'a', 'b', 'c' };
+
+	static A* a;
+	A() {};
+
+public:
+	static A* GetA() {
+		if (a == nullptr) {
+			cout << "123" << endl;
+			a = new A();
+		}
+			
+		return a;
+	}
+
+	void show() {
+		cout << c[0] << endl;
+		cout << c[1] << endl;
+		cout << c[2] << endl;
+	}
+};
+
+A* A::a = nullptr;
+
 
 
 int main(void)
 {
+	A* a = A::GetA();
 
+	a->show();
 
 
 	return 0;
