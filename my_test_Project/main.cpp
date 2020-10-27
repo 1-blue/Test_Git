c434642eb75c9fd6983efc22411930c71c0bc308
1-blue <ghksaud55@naver.com>
2020-10-27 22:56:12 +0900

    자료구조 9주차

--- my_test_Project/main.cpp
+++ my_test_Project/main.cpp
@@ -1,40 +1,52 @@
 #include <iostream>
+#include <string>
+#include <Windows.h>
+#include <map>
 
 using namespace std;
 
-class A {
-	char c[3] = { 'a', 'b', 'c' };
-
-	static A* a;
-	A() {};
-
-public:
-	static A* GetA() {
-		if (a == nullptr) {
-			cout << "123" << endl;
-			a = new A();
-		}
-			
-		return a;
-	}
-
-	void show() {
-		cout << c[0] << endl;
-		cout << c[1] << endl;
-		cout << c[2] << endl;
-	}
-};
-
-A* A::a = nullptr;
+void ReadFileString(const char* section, const char* key, map<string, string>& test, const char* path)
+{
+	TCHAR loadData[1024];
+	GetPrivateProfileString(section, key, "", loadData, 1024, path);
+	test[key] = loadData;
+	setlocale(LC_ALL, "ko_KR.UTF8");
+	cout << loadData << endl;
+	setlocale(LC_ALL, "");
+}
 
 
 
 int main(void)
 {
-	A* a = A::GetA();
+	map <string, string> title;
+	map <string, string> select;
 
-	a->show();
+	ReadFileString("title", "0", title, "GameInfo\\GameStartUI.ini");
+	ReadFileString("title", "1", title, "GameInfo\\GameStartUI.ini");
+	ReadFileString("title", "2", title, "GameInfo\\GameStartUI.ini");
+	ReadFileString("title", "3", title, "GameInfo\\GameStartUI.ini");
+	ReadFileString("title", "4", title, "GameInfo\\GameStartUI.ini");
 
+	ReadFileString("select", "0", select, "GameInfo\\GameStartUI.ini");
+	ReadFileString("select", "1", select, "GameInfo\\GameStartUI.ini");
+	ReadFileString("select", "2", select, "GameInfo\\GameStartUI.ini");
 
+	//string s;
+	//for (int i = 0; i < title.size(); i++)
+	//{
+	//	string s = to_string(i);
+	//	cout << title[s] << endl;
+	//}
+
+	//for (int i = 0; i < select.size(); i++)
+	//{
+	//	string s = to_string(i);
+	//	cout << select[s] << endl;
+	//}
+	string str;
+	cin.get(str, 10, 'i');
+
+	cout << "ÇÑ±Û" << endl;
 	return 0;
 }
---
+++ my_test_Project/자료구조_연결리스트.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//포인터
+	//정적메모리할당
+	//동적메모리할당
+	//링크드리스트
+
+
+
+	return 0;
+}
