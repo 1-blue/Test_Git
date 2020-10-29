dfc3f3468028875887ab3c9fe785f9273288c03e
1-blue <ghksaud55@naver.com>
2020-10-29 19:55:09 +0900

    test

--- test_project/main.cpp
+++ test_project/main.cpp
@@ -2,18 +2,11 @@
 
 using namespace std;
 
+
+
 int main(void)
 {
-	string str = "abcdefg";
 
-	for (int x = 0, y = str.size() - 1; x < y; x++, y--)
-	{
-		char temp = str[y];
-		str[y] = str[x];
-		str[x] = temp;
-	}
-
-	cout << str << endl;
 
 
 	return 0;
