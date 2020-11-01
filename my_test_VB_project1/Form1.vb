ecfe23e52376e698afc35a61c7488001350cd45f
1-blue <ghksaud55@naver.com>
2020-11-01 12:03:38 +0900

    비주얼베이직 공부

---
+++ my_test_VB_project1/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private arr(2) As String
+    Private x As Integer = 1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        For i = 0 To x * 2
+            ReDim arr(x * 2)
+            arr(i) = i
+        Next
+
+        For i = 0 To x * 2
+            lbox_test.Items.Add(CStr(i))
+        Next
+        x += 1
+    End Sub
+End Class
--- test_project/main.cpp
+++ test_project/main.cpp
@@ -2,10 +2,27 @@
 
 using namespace std;
 
-
-
 int main(void)
 {
+	char arr[5];
+	int i = 0;
+	bool a = false;
+
+	while (i != 5)
+	{
+		cin.get(arr[i++]);
+	}
+
+	for (auto k : arr)
+	{
+		cout << k << endl;
+	}
+
+	while (!cin.eof())
+	{
+		cout << cin.get() << endl;
+	}
+
 
 
 
