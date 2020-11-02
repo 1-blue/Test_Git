fe8c0c1e68dc57c9af551154e9208330b1aee18e
1-blue <ghksaud55@naver.com>
2020-11-02 23:53:58 +0900

    test

--- test_project/main.cpp
+++ test_project/main.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
+void WriteData(string tempSection, string tempKey, string tempValue, string tempPath)
+{
+	auto section = tempSection.c_str();
+	auto key = tempKey.c_str();
+	auto value = tempValue.c_str();
+	auto path = tempPath.c_str();
+	WritePrivateProfileString(section, key, value, path);
+}
+
 int main(void)
 {
-	char arr[5];
-	int i = 0;
-	bool a = false;
-
-	while (i != 5)
-	{
-		cin.get(arr[i++]);
-	}
-
-	for (auto k : arr)
-	{
-		cout << k << endl;
-	}
-
-	while (!cin.eof())
-	{
-		cout << cin.get() << endl;
-	}
+	WriteData("player1", "hp", "5", "gameInfo\\player33.ini");
+	WriteData("player1", "power", "3", "gameInfo\\player33.ini");
 
 
 
