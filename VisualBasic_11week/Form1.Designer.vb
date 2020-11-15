352e080fbf9daa3f207767ffe225bc77c8519d61
1-blue <ghksaud55@naver.com>
2020-11-15 15:11:11 +0900

    자바스크립트, VB

---
+++ VisualBasic_11week/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DirListBox1 = New Microsoft.VisualBasic.Compatibility.VB6.DirListBox()
+        Me.DriveListBox1 = New Microsoft.VisualBasic.Compatibility.VB6.DriveListBox()
+        Me.FileListBox1 = New Microsoft.VisualBasic.Compatibility.VB6.FileListBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'DirListBox1
+        '
+        Me.DirListBox1.FormattingEnabled = True
+        Me.DirListBox1.IntegralHeight = False
+        Me.DirListBox1.Location = New System.Drawing.Point(139, 79)
+        Me.DirListBox1.Name = "DirListBox1"
+        Me.DirListBox1.Size = New System.Drawing.Size(120, 96)
+        Me.DirListBox1.TabIndex = 0
+        '
+        'DriveListBox1
+        '
+        Me.DriveListBox1.FormattingEnabled = True
+        Me.DriveListBox1.Location = New System.Drawing.Point(139, 34)
+        Me.DriveListBox1.Name = "DriveListBox1"
+        Me.DriveListBox1.Size = New System.Drawing.Size(121, 22)
+        Me.DriveListBox1.TabIndex = 1
+        '
+        'FileListBox1
+        '
+        Me.FileListBox1.FormattingEnabled = True
+        Me.FileListBox1.Location = New System.Drawing.Point(139, 198)
+        Me.FileListBox1.Name = "FileListBox1"
+        Me.FileListBox1.Pattern = "*.*"
+        Me.FileListBox1.Size = New System.Drawing.Size(120, 88)
+        Me.FileListBox1.TabIndex = 2
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.FileListBox1)
+        Me.Controls.Add(Me.DriveListBox1)
+        Me.Controls.Add(Me.DirListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DirListBox1 As Compatibility.VB6.DirListBox
+    Friend WithEvents DriveListBox1 As Compatibility.VB6.DriveListBox
+    Friend WithEvents FileListBox1 As Compatibility.VB6.FileListBox
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+End Class
