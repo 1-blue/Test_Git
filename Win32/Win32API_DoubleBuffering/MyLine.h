30c51cc143dae80e80342c489e5e33618427f1eb
1-blue <ghksaud55@naver.com>
2020-11-25 01:28:33 +0900

    Shape이동.. timer delete문제있음..

---
+++ Win32/Win32API_DoubleBuffering/MyLine.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "MyShape.h"
+
+class MyLine : public MyShape
+{
+
+};
+
--- Win32/Win32API_DoubleBuffering/Win32API_DoubleBuffering.cpp
+++ Win32/Win32API_DoubleBuffering/Win32API_DoubleBuffering.cpp
@@ -1,9 +1,12 @@
-﻿// Win32API__.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+﻿// Win32API_DoubleBuffering.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
 #include "framework.h"
-#include "Win32API__.h"
+#include "Win32API_DoubleBuffering.h"
 #include <stdio.h>
+#include "MyShape.h"
+#include "MyRect.h"
+#include "MyCircle.h"
 
 #define MAX_LOADSTRING 100
 
@@ -11,14 +14,19 @@
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
-HWND hWnd;
-RECT rc{ 0, 0, 0, 0 };
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+
+//내맘대로 추가
+HWND hWnd;  //요거해결
+vector<MyShape*> shape;
+RECT clientRect{ 0, 0, 0, 0 };  //클라이언트영역크기
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -32,7 +40,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_WIN32API, szWindowClass, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_WIN32APIDOUBLEBUFFERING, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
     // 애플리케이션 초기화를 수행합니다:
@@ -40,73 +48,52 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     {
         return FALSE;
     }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WIN32API));
-
     MSG msg;
-    bool running = true;
-    static RECT r{ 0, 0, 100 ,100 };
-    static RECT r2{ 100, 100, 200 ,200 };
-    static char str[10];
 
-    while (true)
+    while(true)
     {
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))       //메시지있으면실행
+        if (PeekMessage(&msg, hWnd, 0, 0, PM_REMOVE))
         {
             if (msg.message == WM_QUIT)
-                return 0;
-            else
-            {
-                TranslateMessage(&msg);
-                DispatchMessage(&msg);
-            }
+                break;
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
         }
-        else        //메시지 없으면 실행    
+        else
         {
-            GetClientRect(hWnd, &rc);
-            HDC hdc = GetDC(hWnd);
-            HDC mdc = CreateCompatibleDC(hdc);
-            HBITMAP mBitmap = CreateCompatibleBitmap(mdc, rc.right, rc.bottom);
-
-            SelectObject(mdc, mBitmap);
-            FillRect(mdc, &rc, (HBRUSH)GetStockObject(WHITE_BRUSH)); //도화지 색 변경
-
-            r.left++;
-            r.right++;
-
-            if (r.left >= rc.right)
-            {
-                r.left = 0;
-                r.right = 100;
-            }
-
             switch (msg.message)
             {
-            case WM_KEYDOWN:
-                if (GetAsyncKeyState(VK_RIGHT))
-                {
-                    r2.left++;
-                    r2.right++;
-                }
-                if (GetAsyncKeyState(VK_LEFT))
-                {
-                    r2.left--;
-                    r2.right--;
-                }
-                Rectangle(mdc, r2.left, r2.top, r2.right, r2.bottom);
+            case WM_TIMER:
+                shape[msg.wParam]->Update();
+                shape[msg.wParam]->Render();
+                //switch (msg.wparam)
+                //{
+                //case 0:
+                //    shape[0]->update();
+                //    shape[0]->render();
+                //    break;
+                //case 1:
+                //    shape[1]->update();
+                //    shape[1]->render();
+                //    break;
+                //case 2:
+                //    shape[2]->update();
+                //    shape[2]->render();
+                //    break;
+                //}
                 break;
             }
-
-            Rectangle(mdc, r.left, r.top, r.right, r.bottom);
-            Rectangle(mdc, r2.left, r2.top, r2.right, r2.bottom);
-
-            BitBlt(hdc, 0, 0, rc.right, rc.bottom, mdc, 0, 0, SRCCOPY);
-
-            DeleteObject(mBitmap);
-            DeleteDC(mdc);
-            ReleaseDC(hWnd, hdc);
+            //for (int i = 0; i < shape.size(); i++)
+            //{
+            //    shape[i]->Update();
+            //    shape[i]->Render();
+            //}
+            shape[0]->MyBitBlt();
         }
     }
+
+    for (int i = 0; i < shape.size(); i++)
+        shape[i]->Destroy();
 
     return (int) msg.wParam;
 }
@@ -117,17 +104,17 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
     wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32API));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WIN32API);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32APIDOUBLEBUFFERING));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WIN32APIDOUBLEBUFFERING);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
 }
@@ -152,29 +139,19 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    static RECT r{ 100, 100, 200, 200 };
+    static POINT p{ 0, 0 };
+    static char str[100]{ '\0' };
 
     switch (message)
     {
-    case WM_KEYDOWN:
-        //if (wParam == VK_RIGHT)
-        //{
-        //    HDC hdc = GetDC(hWnd);
-        //    HDC mdc = CreateCompatibleDC(hdc);
-        //    HBITMAP hBitmap = CreateCompatibleBitmap(mdc, rc.right, rc.bottom);
-        //    SelectObject(mdc, hBitmap);
-        //    FillRect(mdc, &rc, (HBRUSH)GetStockObject(WHITE_BRUSH)); //도화지 색 변경
+    case WM_CREATE:
+        GetClientRect(hWnd, &clientRect);       //일단 사이즈변경하면안됨
 
-        //    Rectangle(mdc, r.left, r.top, r.right, r.bottom);
-        //    r.left++;
-        //    r.right++;
-
-        //    BitBlt(hdc, 0, 0, rc.right, rc.bottom, mdc, 0, 0, SRCCOPY);
-
-        //    DeleteObject(hBitmap);
-        //    ReleaseDC(hWnd, mdc);
-        //    ReleaseDC(hWnd, hdc);
-        //}
+        shape.emplace_back(new MyRect(hWnd, 0, 50, RECT{ 100, 100 * 0, 200, 100 + 100 * 0 }, clientRect));
+        shape[0]->Initialize();
+        shape.emplace_back(new MyCircle(hWnd, 1, 33, RECT{ 100, 100 * 1, 200, 100 + 100 * 1 }, clientRect));
+        shape[1]->Initialize();
+            
         break;
 
     case WM_COMMAND:
@@ -198,6 +175,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
+            
 
             EndPaint(hWnd, &ps);
         }
